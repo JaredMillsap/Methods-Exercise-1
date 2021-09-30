74f4fede421b3a140c769e06ac6e383110255ac9
JaredMillsap <jmillsap23@gmail.com>
2021-09-30 11:57:40 -0500

    Excercise 1 Calling methods

---
+++ Methods/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code that will be placed inside of method
+
+            MyIntro();
+            
+        }
+
+        // In order to create a method there must be * Class (
+
+        public static void MyIntro()
+        {
+            string firstname = "Jared";
+            string lastName = "Millsap";
+            string Age = "25";
+
+
+            string myIntro = $" My name is {firstname} {lastName}, I am {Age} years old! ";
+
+            Console.WriteLine(myIntro);
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////Scope- 
+//string firstname = "Jared";
+//string lastName = "Millsap";
+//string Age = "25";
+
+
+//string myIntro = $" My name is {firstname} {lastName}, I am {Age} years old! ";
